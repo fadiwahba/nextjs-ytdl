32b10e41c8d1fa2d36a01ba2c038d53dd6f2319e
Fady Soliman <fadi.wahba@gmail.com>
2025-01-29 12:28:03 +1300

    fixed UI toast component and hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 import DownloadForm from "@/components/download-form";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-2xl font-bold mb-8">YouTube Video Downloader</h1>
-      <DownloadForm />
+    <main className="flex min-h-screen flex-col items-center justify-start p-24 bg-slate-100">
+      <Card className="space-y-4 max-w-md mx-auto shadow-lg">
+        <CardHeader>
+            <h1 className="text-2xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-pink-500 to-orange-500">
+            YouTube Downloader
+            </h1>
+        </CardHeader>
+        <CardContent>
+          <DownloadForm />
+        </CardContent>
+      </Card>
     </main>
   );
 }
